7785608dd77c56e728f3e55b6bb16fec98528394
smtinu <tinusm1980@gmail.com>
2017-07-28 15:03:23 +0530

    pocnow

---
+++ edge-proxy/src/main/java/com/microservice/EdgeProxyApplication.java
@@ -0,0 +1,16 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient	
+public class EdgeProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeProxyApplication.class, args);
+	}
+}
